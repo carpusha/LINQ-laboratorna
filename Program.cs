536bc630744de235ef9de597b4645da9734157ed
carpusha <blerdlol123@gmail.com>
2024-03-05 18:17:44 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿char C = 'c';
+string[] A = { "abc", "dea", "gha" };
+
+try
+{
+    var result = A.Where(s => s.EndsWith(C)).SingleOrDefault();
+
+    if (A.Count(s => s.EndsWith(C)) == 0)
+    {
+        Console.WriteLine("");
+    }
+    else if (A.Count(s => s.EndsWith(C)) == 1)
+    {
+        Console.WriteLine(result);
+    }
+    else
+    {
+        Console.WriteLine("Error");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Виникла помилка" + ex.Message);
+}
+
+Console.ReadKey();
